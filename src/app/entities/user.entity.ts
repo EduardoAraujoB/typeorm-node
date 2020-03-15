b5b5b2fb1fc10057b55d79aa5c2ce607a4d25725
EduardoAraujoB <ea0123450@gmail.com>
2020-03-15 18:41:19 -0300

    created user entity

---
+++ src/app/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Unique, Column } from 'typeorm';
+
+@Entity()
+@Unique(['email'])
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password_hash: string;
+}
