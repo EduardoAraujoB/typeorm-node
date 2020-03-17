b9efdf3779aa1a2d8ecfffd8a50cfe01bab4dc42
EduardoAraujoB <ea0123450@gmail.com>
2020-03-17 15:50:30 -0300

    using the different envoronment variables and database for tests

--- src/database/Connection.ts
+++ src/database/Connection.ts
@@ -1,9 +1,11 @@
 import { createConnection, Connection } from 'typeorm';
 import { User } from '../app/entities/user.entity';
 
+import '../bootstrap';
+
 export async function databaseConnection(): Promise<Connection> {
   const connection = await createConnection({
-    type: 'postgres',
+    type: process.env.NODE_ENV !== 'test' ? 'postgres' : 'sqlite',
     host: process.env.DB_HOST,
     port: +process.env.DB_PORT,
     username: process.env.DB_USERNAME,
