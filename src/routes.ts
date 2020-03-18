068233043895d1162ca0be033594138a8e6b71bd
EduardoAraujoB <ea0123450@gmail.com>
2020-03-18 15:45:00 -0300

    created route to create a new user

--- src/routes.ts
+++ src/routes.ts
@@ -5,5 +5,6 @@ import UserController from './app/controllers/UserController';
 const routes = Router();
 
 routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
 
 export default routes;
