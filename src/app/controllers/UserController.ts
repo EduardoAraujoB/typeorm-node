1f2189dcb28a38c41af7f2ab96dc230876f3e120
EduardoAraujoB <ea0123450@gmail.com>
2020-03-18 15:44:15 -0300

    created method to store a new user

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -9,6 +9,13 @@ class UserController {
     const users = await userRepository.find();
     return res.json(users);
   }
+
+  public async store(req: Request, res: Response): Promise<Response> {
+    const userRepository = getManager().getRepository(User);
+    const newUser = userRepository.create(req.body);
+    const createdUser = await userRepository.save(newUser);
+    return res.json(createdUser);
+  }
 }
 
 export default new UserController();
