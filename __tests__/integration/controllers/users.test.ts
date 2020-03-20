4dbad81ce238c3f22a81551a99b6eb98354c34ae
EduardoAraujoB <ea0123450@gmail.com>
2020-03-20 16:27:57 -0300

    created test for invalid schemas and duplicated emails

--- __tests__/integration/controllers/users.test.ts
+++ __tests__/integration/controllers/users.test.ts
@@ -36,4 +36,29 @@ describe('User', () => {
 
     expect(response.status).toBe(200);
   });
+
+  it('should return an error with a invalid request body', async () => {
+    const response = await request(app.express)
+      .post('/users')
+      .send({
+        name: 'person',
+        email: 'person@mail.com',
+      });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should not be able to store a user with a duplicated email', async () => {
+    const user: object = await factory.attrs('User');
+
+    await request(app.express)
+      .post('/users')
+      .send(user);
+
+    const response = await request(app.express)
+      .post('/users')
+      .send(user);
+
+    expect(response.status).toBe(400);
+  });
 });
