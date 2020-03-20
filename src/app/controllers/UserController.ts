4e4703b612c3af82bd757a43561fac77bee18202
EduardoAraujoB <ea0123450@gmail.com>
2020-03-20 16:26:58 -0300

    created schema validation and verification for duplicated emails

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { getManager } from 'typeorm';
+import * as Yup from 'yup';
 
 import { User } from '../entities/user.entity';
 
@@ -11,7 +12,28 @@ class UserController {
   }
 
   public async store(req: Request, res: Response): Promise<Response> {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Schema is not valid' });
+    }
+
     const userRepository = getManager().getRepository(User);
+
+    const checkUserExists = await userRepository.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (checkUserExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
     const newUser = userRepository.create(req.body);
     const createdUser = await userRepository.save(newUser);
     return res.json(createdUser);
