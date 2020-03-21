7e2a7e6a05f27002c51f6ef45107153339739010
EduardoAraujoB <ea0123450@gmail.com>
2020-03-21 15:36:54 -0300

    created route to store a new session

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = Router();
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
