f9a49fa55439bc2293837e96abe45e94f0e78c32
EduardoAraujoB <ea0123450@gmail.com>
2020-03-22 21:17:10 -0300

    created provisional route to test authentication middleware

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,17 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = Router();
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get('/hello', (req, res) => res.json('ok'));
 
 export default routes;
